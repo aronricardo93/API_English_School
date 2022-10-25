b24da81d3e1a5086f14e09583bae3e6adf9b2516
aronricardo93 <aronricardo@hotmail.com>
2022-10-25 13:56:23 -0300

    add novas rotas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const pessoas = require('./pessoasRoute')
+const turmas = require('./turmasRoute')
+const niveis = require('./niveisRoute')
 
 module.exports = app =>{
     app.use(express.json())
-    app.use(pessoas)
+    app.use(pessoas, turmas, niveis)
 }

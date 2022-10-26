6c48215aba48a19d173d1953d350f0778b33bb5b
aronricardo93 <aronricardo@hotmail.com>
2022-10-26 13:43:04 -0300

    add rotas matriculas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,5 +8,9 @@ router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criarPessoa)
 router.put('/pessoas/:id', PessoaController.atualizarPessoa)
 router.delete('/pessoas/:id', PessoaController.deletarPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId/', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId/', PessoaController.atualizarMatricula)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletarMatricula)
 
 module.exports = router
